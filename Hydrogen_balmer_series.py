cfda964b6b9499c98a57767f2858daad074052b8
Anuj-Arya1 <anuj.arya@niser.ac.in>
2025-09-14 12:12:01 +0530

    hydrogen_exp

---
+++ Hydrogen_balmer_series.py
@@ -0,0 +1,114 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from prettytable import PrettyTable
+
+# Sample data
+
+Vernier1_L =[168,167,164.883,163.066,161.683,161.283,160.65,160.333]
+Vernier1_R =[196.25,197.416,199.5,201.5,202.683,203.233,204.083,204.283]
+Vernier2_L =[347.5,346.5,344.716,342.7,341.7,340.566,340.166,340]
+Vernier2_R =[15.85,17,19.166,21.233,22.5,23,23.833,24]
+V1_theta = [(Vernier1_R[i]-Vernier1_L[i]) for i in range(len(Vernier1_L))]
+V2_theta = [(360+(Vernier2_R[i]-Vernier2_L[i])) for i in range(len(Vernier2_L))]
+theta_avg = [(V1_theta[i]+V2_theta[i])/4 for i in range(len(V1_theta))]
+# print(V1_theta,'\n')
+# print(V2_theta,'\n')
+# print(theta_avg,'\n')
+sin_theta = [math.sin(math.radians(theta)) for theta in theta_avg]
+# print(sin_theta)
+lambda_ = [413,448,501,560,588,614,627,637]  # in nm
+X = sin_theta
+Y = lambda_
+
+
+
+# Vernier1_L =[158.716,165,166.833]
+# Vernier1_R =[205.166,199,197]
+# Vernier2_L =[338.5,344.5,346.5]
+# Vernier2_R =[24.966,18.566,16.5]
+
+# V1_theta = [(Vernier1_R[i]-Vernier1_L[i]) for i in range(len(Vernier1_L))]
+# V2_theta = [(360+(Vernier2_R[i]-Vernier2_L[i])) for i in range(len(Vernier2_L))]
+# theta_avg = [(V1_theta[i]+V2_theta[i])/4 for i in range(len(V1_theta))]
+# print(V1_theta,'\n')
+# print(V2_theta,'\n')
+# print(theta_avg,'\n')
+
+# Y = [1.492,2.011,2.268] # 1/lambda (in 10^6 / m)
+# X = [0.1389,0.1875,0.2100]  # 1/m^2 -1/n^2
+# TABLE DATA
+Σx2 = 0
+Σy = 0
+Σxy = 0
+Σx = 0
+count = 0
+Σxylist = []
+Σx2list = []
+Slno = [ ]
+
+for i in range(len(X)):
+    Σx2 += X[i]**2
+    Σx2list.append(round(X[i]**2,4))
+    Σy += Y[i]
+    Σxy += X[i]*Y[i]
+    Σxylist.append(round(X[i]*Y[i],4))
+    Σx += X[i]
+    count += 1
+    Slno.append(count)
+
+# display table(least square fitting)
+table = PrettyTable()
+table.field_names = ["Sl No.","X", "Y", "X^2", "XY"]
+table.add_rows(list(zip(Slno, X, Y, Σx2list, Σxylist)))
+table.add_row([" ",'','','',''])
+table.add_row(["Total -", round(Σx,4), round(Σy,4), round(Σx2,4), round(Σxy,4)])
+print(table)
+
+# Finding slope and intercept
+fit = np.polyfit(X,Y,1)
+A = fit[0]
+B = fit[1]
+def f(x):
+  return A*x + B
+# Error analysis
+error_sum = 0
+for i in range(len(X)):
+    error_sum += (Y[i] - f(X[i]))**2
+error_y = (error_sum/ (len(X) - 2))**0.5
+delta = len(X)*Σx2 - Σx**2
+error_slope = error_y * (len(X)/delta)**0.5
+error_intercept = error_y * (Σx2/delta)**0.5
+
+#Printing slopes,intercepts and errors
+table2 = PrettyTable()
+table2.title = "Slope, Intercept and Errors"
+table2.add_row(["Slope" , round(A,3) ])
+table2.add_row(["Intercept" , round(B,3) ])
+table2.add_row(["Error in y", round(error_y,3) ])
+table2.add_row(["Delta" , round(delta,5) ])
+table2.add_row(["Error in slope", round(error_slope,5) ])
+table2.add_row(["Error in intercept", round(error_intercept,5) ])
+print(table2)
+print()
+
+#Printing the function values
+table3 = PrettyTable(["x" , "y = a_0 + a_1 x"])
+table3.title ="Function values"
+for i in X:
+    table3.add_row([i, round(f(i),3)])
+print(table3)
+X_axis = r'$1/m^2 - 1/n^2$'
+Y_axis = r'$1/\lambda$ (in $10^6 / \text{m}$)'
+#GRAPH PLOT
+plt.xlabel(X_axis)
+plt.ylabel(Y_axis)
+plt.title(X_axis + " vs " + Y_axis)
+plt.grid()
+plt.scatter(X,Y,label='Data Points',s=20,c='orange')
+fit = np.polyfit(X,Y,1)
+x_fit = np.linspace(min(X),max(X),1000)
+y_fit = fit[1]+fit[0]*x_fit
+plt.plot(x_fit,y_fit,'--', label = f'Least-square fit line : y = ({round(A,3)}±{round(error_slope,3)})x + ({round(B,3)}±{round(error_intercept,3)})')
+plt.legend()
+plt.show()
